722bd3073164882e1f12ae521c807e5c6bd0f13e
unknown <gonzalanfranchi@gmail.com>
2023-03-20 21:51:37 -0300

    Clases creadas...

---
+++ ProyectoPractica/Disco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPractica
+{
+    internal class Disco
+    {
+        public int IdDisco { get; set; }
+        public string Titulo { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
+        public int CantidadCanciones { get; set; }
+        public string UrlImagenTapa { get; set; }
+        public int IdEstilo { get; set; }
+        public int IdEdicion { get; set; }
+    }
+}
---
+++ ProyectoPractica/Estilo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPractica
+{
+    internal class Estilo
+    {
+        public int IdEstilo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
